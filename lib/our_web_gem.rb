a219883d6ad089e3ab4111fbb2a3c1d1e87dd642
vkhokh <vladkhokhlov220606@gmail.com>
2026-03-25 00:16:58 +0300

    add implementation

--- lib/our_web_gem.rb
+++ lib/our_web_gem.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "our_web_gem/version"
+require_relative "our_web_gem/renderer"
 
 module OurWebGem
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.render(ast)
+    Renderer.new.render(ast)
+  end
 end
--- spec/our_web_gem_spec.rb
+++ spec/our_web_gem_spec.rb
@@ -1,11 +1,68 @@
 # frozen_string_literal: true
 
-RSpec.describe OurWebGem do
-  it "has a version number" do
-    expect(OurWebGem::VERSION).not_to be nil
+require_relative "../lib/our_web_gem"
+
+RSpec.describe OurWebGem::Renderer do
+  subject(:renderer) { described_class.new }
+
+  it "renders heading" do
+    ast = [
+      {
+        type: :heading,
+        level: 1,
+        children: [{ type: :text, value: "Hello" }]
+      }
+    ]
+
+    expect(renderer.render(ast)).to eq("<h1>Hello</h1>")
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "renders inline code" do
+    ast = [
+      {
+        type: :paragraph,
+        children: [
+          { type: :text, value: "Use " },
+          { type: :code_inline, value: "puts" }
+        ]
+      }
+    ]
+
+    expect(renderer.render(ast)).to eq("<p>Use <code>puts</code></p>")
   end
+
+  it "renders code block" do
+    ast = [
+      {
+        type: :code_block,
+        value: "puts 'Hello'"
+      }
+    ]
+
+    expect(renderer.render(ast)).to eq("<pre><code>puts 'Hello'</code></pre>")
+  end
+
+  it "renders blockquote" do
+    ast = [
+      {
+        type: :blockquote,
+        children: [
+          {
+            type: :paragraph,
+            children: [{ type: :text, value: "Quote" }]
+          }
+        ]
+      }
+    ]
+
+    expect(renderer.render(ast)).to eq("<blockquote><p>Quote</p></blockquote>")
+  end
+  it "renders empty input" do
+  expect(renderer.render([])).to eq("")
+end
+
+it "raises on unknown node" do
+  ast = [{ type: :unknown }]
+  expect { renderer.render(ast) }.to raise_error(ArgumentError)
+end
 end
